60b742ef35b26409ba87ca42e989ba56984a9f53
Ayana2209 <147966257+Ayana2209@users.noreply.github.com>
2023-10-23 00:47:45 +0600

    Add files via upload

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -5,12 +5,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/category/', views.category_list_api_view),
-    path('api/v1/category/<int:id>/', views.category_detail_api_view),
+    path('api/v1/category/<int:category_id>/', views.category_detail_api_view),
 
     path('api/v1/products/', views.product_list_api_view),
-    path('api/v1/products/<int:id>/', views.product_derail_api_view),
+    path('api/v1/products/<int:product_id>/', views.product_derail_api_view),
+    path('api/v1/products/reviews/', views.product_review_list),
 
     path('api/v1/review/', views.review_list_api_view),
-    path('api/v1/review/<int:id/>', views.review_detail_api_view),
+    path('api/v1/review/<int:review_id>/', views.review_detail_api_view),
 
 ]
