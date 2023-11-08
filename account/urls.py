411c6492d5b871b2e7bafb4041faf10a46adf0c6
Ayana2209 <147966257+Ayana2209@users.noreply.github.com>
2023-11-08 20:39:04 +0600

    Delete account directory

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.registration_user_api_view),
-    path('login/', views.login_user_api_view),
-    path('confirm/', views.confirm_user_api_view),
-    ]
